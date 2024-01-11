906c7e72dd9feef0a82fb900a9d4282efa00cc48
Moomi98 <kymkjh2002@gmail.com>
2024-01-11 21:25:28 +0900

    페이지 사이즈 고정

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <GlobalStyles />
         <StyledComponentsProvider>
           <Header />
-          {children}
+          <main>{children}</main>
         </StyledComponentsProvider>
       </body>
     </html>
