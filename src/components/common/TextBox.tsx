27a81c3a0acb8424f19ffa725f6e9a985f2da5e6
Moomi98 <kymkjh2002@gmail.com>
2024-02-15 20:32:33 +0900

    TextBox 컴포넌트에 fontWeight, fontSize props 추가

--- src/components/common/TextBox.tsx
+++ src/components/common/TextBox.tsx
@@ -1,44 +1,73 @@
 "use client";
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { MdClear } from "react-icons/md";
-import { ChangeEvent, MouseEventHandler, useState } from "react";
+import { ChangeEvent, useState } from "react";
 
-const Container = styled.div<{ width: string }>`
-  width: 100%;
-  height: 2rem;
-  border-radius: 0.25rem;
-  border: 1px solid black;
-  padding: 5px 10px;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  height: 100%;
-  border: none;
-  outline: none;
-`;
-
-const ClearButton = styled(MdClear)`
-  cursor: pointer;
-`;
+type TFontSize = "small" | "medium" | "large" | "xlarge";
 
 type TextBoxProps = {
   width?: string;
   showClearButton?: boolean;
   value?: string;
   onChange?: Function;
+  placeholder?: string;
+  fontWeight?: number;
+  limit?: number;
+  fontSize?: TFontSize;
 };
 
+const fontSizeMapper = {
+  small: "0.75rem",
+  medium: "1rem",
+  large: "2rem",
+  xlarge: "3rem",
+};
+
+const Container = styled.div<{ $width: string }>`
+  width: ${(props) => props.$width ?? css`"100%"`};
+  min-height: 2rem;
+  border-radius: 0.25rem;
+  border: 1px solid black;
+  padding: 8px 12px;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const Input = styled.input<{
+  $fontWeight: number;
+  $fontSize: TFontSize;
+}>`
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  font-weight: ${(props) => props.$fontWeight ?? 500};
+  font-size: ${(props) =>
+    props.$fontSize ? fontSizeMapper[props.$fontSize] : "1rem"};
+`;
+
+const ClearButton = styled(MdClear)`
+  cursor: pointer;
+`;
+
 export default function TextBox(props: TextBoxProps) {
-  const { width = "100%", showClearButton = true, value = "" } = props;
+  const {
+    width = "100%",
+    showClearButton = true,
+    value = "",
+    placeholder = "",
+    fontWeight = 500,
+    limit,
+    fontSize = "medium",
+  } = props;
 
   const [currentValue, setCurrentValue] = useState(value);
 
-  const setText = (event: ChangeEvent<HTMLInputElement>) => {
+  const onValueChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    if (limit && value.length > limit) return;
+
     setCurrentValue(event.target.value);
     props.onChange && props.onChange(event.target.value);
   };
@@ -49,8 +78,14 @@ export default function TextBox(props: TextBoxProps) {
   };
 
   return (
-    <Container width={width}>
-      <Input value={currentValue} onChange={setText} />
+    <Container $width={width}>
+      <Input
+        value={currentValue}
+        onChange={onValueChanged}
+        placeholder={placeholder}
+        $fontWeight={fontWeight}
+        $fontSize={fontSize}
+      />
       {showClearButton && currentValue.length > 0 && (
         <ClearButton size={"1.5rem"} onClick={clearText} />
       )}
